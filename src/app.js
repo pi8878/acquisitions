0f2a035857a4fc2f15c63aa8140eae1faac734ad
Simangolwa <simangolwalifwatila99@gmail.com>
2026-03-19 18:02:12 -0400

    Implement logger, morgan, and helmet for http

--- src/app.js
+++ src/app.js
@@ -6,11 +6,26 @@
 // With these changes, we should be able to run our express server without any issues.
 
 import express from 'express';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from aquisitions!');
+  logger.info('Hello from Acquisitions!');
+  res.status(200).send('Hello from Acquisitions!');
 });
 
 export default app;
