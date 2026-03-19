238742c646ba01dbfe99532f9bb99f2928fc7c00
Simangolwa <simangolwalifwatila99@gmail.com>
2026-03-19 16:17:14 -0400

    implement Eslint and Prettier

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-// All about making sure everything is set up correctly to run the express server without any issues. This includes setting the "type": 
-// module" field in the package.json file, using the correct file extension for modules, and making sure that the imports 
+// All about making sure everything is set up correctly to run the express server without any issues. This includes setting the "type":
+// module" field in the package.json file, using the correct file extension for modules, and making sure that the imports
 // and exports are done correctly. With these changes, we should be able to run our express server without any issues.
 
 import app from './app.js';
