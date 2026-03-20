1d6e7109b05d0514acceedbb4b53e1acd3eefe21
Simangolwa <simangolwalifwatila99@gmail.com>
2026-03-20 17:03:36 -0400

    Implement auth

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 import {neon, neonConfig} from '@neondatabase/serverless';
-import {drizzle} from 'drizzle-oram/neon-http';
+import {drizzle} from 'drizzle-orm/neon-http';
 
 const sql = neon(process.env.DATABASE_URL);
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,18 +1,13 @@
+import { signIn, signOut, signup } from '../controllers/auth.controller.js';
 import express from 'express';
 
 const router = express.Router();
 
 
-router.post('/sign-up', (req, res) => {
-    res.send('POST /api/auth/sign-up response');
-});
+router.post('/sign-up', signup)
 
-router.post('/sign-in', (req, res) => {
-    res.send('POST /api/auth/sign-in response');
-});
+router.post('/sign-in', signIn);
 
-router.post('/sign-out', (req, res) => {
-    res.send('POST /api/auth/sign-out response');
-});
+router.post('/sign-out', signOut);
 
 export default router;
