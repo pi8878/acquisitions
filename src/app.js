c058416530629ed1f86de5ad6038ca806c386ca2
Simangolwa <simangolwalifwatila99@gmail.com>
2026-03-21 01:52:56 -0400

    Add secure API with Arcjet

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js'
 
 const app = express();
 
@@ -23,6 +24,8 @@ app.use(cookieParser());
 
 
 app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
